5a924285156dca328b12554e9e30a22253561a6c
Ian Wilson <iwilsonq@gmail.com>
2018-12-08 16:17:26 -0800

    gatsby vars

--- src/services/shopify.js
+++ src/services/shopify.js
@@ -1,8 +1,13 @@
 import Client from 'shopify-buy'
 
+const {
+  GATSBY_SHOPIFY_DOMAIN,
+  GATSBY_SHOPIFY_STOREFRONT_ACCESS_TOKEN
+} = process.env
+
 const client = Client.buildClient({
-  domain: process.env.SHOPIFY_DOMAIN,
-  storefrontAccessToken: process.env.SHOPIFY_STOREFRONT_ACCESS_TOKEN
+  domain: GATSBY_SHOPIFY_DOMAIN,
+  storefrontAccessToken: GATSBY_SHOPIFY_STOREFRONT_ACCESS_TOKEN
 })
 
 export default client
