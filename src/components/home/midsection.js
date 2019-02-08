e5bfd8faa377c0660fbe90dd77daa2b892af5d04
Ian Wilson <iwilsonq@gmail.com>
2019-02-07 19:30:02 -0800

    style(home): updated with new images

--- src/components/home/midsection.js
+++ src/components/home/midsection.js
@@ -4,6 +4,8 @@ import rishi from '../../images/rishi.png'
 import h1bBoyband from '../../images/h-1b_boyband_1.png'
 import microsoft from '../../images/microsoft.jpg'
 import crossing from '../../images/CROSSING.png'
+import google from '../../images/google_sign_expats.jpg'
+import h1bGang from '../../images/h-1b_gang.png'
 
 const Midsection = () => (
   <section className="section">
@@ -13,7 +15,7 @@ const Midsection = () => (
           <div className="column">
             <article className="media mb-10">
               <div className="midsection-1">
-                <img src={rishi} alt="rishi" />
+                <img src={rishi} alt="rishi" className="mb-16" />
                 <div className="max-width-700">
                   <p className="big-lead center">
                     What does it mean to be American?
@@ -30,8 +32,8 @@ const Midsection = () => (
         </div>
       </div>
       <div className="mb-128">
-        <div className="columns">
-          <div className="column col-5-12">
+        <div className="columns is-flex-mobile">
+          <div className="column col-5-12-tablet col-1-2-mobile">
             <article className="media flex-start mb-10">
               <div className="midsection-2">
                 <figure className="media-left">
@@ -40,7 +42,7 @@ const Midsection = () => (
               </div>
             </article>
           </div>
-          <div className="column col-7-12">
+          <div className="column col-7-12-tablet col-1-2-mobile">
             <article className="media flex-start mb-10">
               <div className="midsection-2">
                 <h3 className="header-3">Ready to Wear</h3>
@@ -56,8 +58,8 @@ const Midsection = () => (
         </div>
       </div>
       <div className="mb-128">
-        <div className="columns">
-          <div className="column col-7-12">
+        <div className="columns is-flex-mobile">
+          <div className="column col-7-12-tablet col-1-2-mobile">
             <article className="media flex-end mb-10">
               <div className="midsection-3 ">
                 <h3 className="header-3 is-right">Who are you?</h3>
@@ -69,7 +71,7 @@ const Midsection = () => (
               </div>
             </article>
           </div>
-          <div className="column col-5-12">
+          <div className="column col-5-12-tablet col-1-2-mobile">
             <article className="media flex-start mb-10">
               <div className="midsection-3">
                 <figure className="media-left">
@@ -82,10 +84,17 @@ const Midsection = () => (
       </div>
       <div className="mb-128">
         <div className="columns">
+          <div className="is-full">
+            <img src={google} alt="google" />
+          </div>
+        </div>
+      </div>
+      <div className="mb-128">
+        <div className="columns">
           <div className="column">
             <article className="media mb-10">
               <div className="midsection-crossing">
-                <img src={crossing} alt="crossing" className="mb-16"/>
+                <img src={crossing} alt="crossing" className="mb-16" />
                 <Link to="/ready-to-wear">
                   <button className="default-button small is-uppercase">
                     Shop the look
@@ -93,6 +102,18 @@ const Midsection = () => (
                 </Link>
               </div>
             </article>
+          </div>
+        </div>
+      </div>
+      <div className="mb-128">
+        <div className="columns">
+          <div className="is-full">
+            <div className="max-width-700 mh-auto">
+              <p className="big-lead center mb-24">
+                What is the American family?
+              </p>
+            </div>
+            <img src={h1bGang} alt="h1b gang" />
           </div>
         </div>
       </div>
